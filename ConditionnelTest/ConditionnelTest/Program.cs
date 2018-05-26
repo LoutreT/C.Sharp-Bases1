d713150c3490142702e8f4651c31014957c36b49
Vincent <v.nassaux@nootos.com>
2018-05-26 02:08:37 +0200

    Conditions-1

---
+++ ConditionnelTest/ConditionnelTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConditionnelTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal compte = 300;
+            bool estCrediteur = (compte >= 0);
+            if (compte >= 0)
+                Console.WriteLine("Votre compte est créditeur");
+            else
+                Console.WriteLine("Votre compte est débiteur");
+        }
+    }
+}
